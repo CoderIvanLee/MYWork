feafe0f0738a98b10c211ecf9ca44cbb29353821
CoderIvanLee <583349285@qq.com>
2015-10-21 21:42:55 +0800

    初始化项目

---
+++ MyWork/MyWork/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyWork
+//
+//  Created by 传智.小飞燕 on 15/10/21.
+//  Copyright © 2015年 itcast. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyWork/MyWork/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyWork
+//
+//  Created by 传智.小飞燕 on 15/10/21.
+//  Copyright © 2015年 itcast. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
